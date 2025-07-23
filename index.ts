9cd4a9648f337e0b6327f657d3e55f917129c603
fauxfire <andyweng97@gmail.com>
2025-07-23 15:36:10 +0800

    add getClosestValidator

--- index.ts
+++ index.ts
@@ -1,23 +1,41 @@
-import { Connection, Transaction, ConfirmOptions, Keypair, TransactionSignature} from "@solana/web3.js";
+import { Connection, Transaction, ConfirmOptions, Keypair, TransactionSignature, PublicKey } from "@solana/web3.js";
 
 // Based on a raw transaction, get the writable accounts
 export function getWritableAccounts(transaction: Transaction) {
   const writableAccounts = new Set<string>();
 
-      if (transaction.feePayer) {
-        writableAccounts.add(transaction.feePayer.toBase58());
-      }
+  if (transaction.feePayer) {
+    writableAccounts.add(transaction.feePayer.toBase58());
+  }
 
-      // Check all instruction keys
-      for (const instruction of transaction.instructions) {
-        for (const key of instruction.keys) {
-          if (key.isWritable) {
-            writableAccounts.add(key.pubkey.toBase58());
-          }
-        }
+  // Check all instruction keys
+  for (const instruction of transaction.instructions) {
+    for (const key of instruction.keys) {
+      if (key.isWritable) {
+        writableAccounts.add(key.pubkey.toBase58());
       }
+    }
+  }
 
-      return Array.from(writableAccounts);
+  return Array.from(writableAccounts);
+}
+
+export async function getClosestValidator(routerConnection: Connection) {
+  const response = await fetch(routerConnection.rpcEndpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'getIdentity',
+      params: []
+    })
+  });
+
+  const identityData = await response.json();
+  const validatorKey = new PublicKey(identityData.result.identity);
+
+  return validatorKey;
 }
 
 export async function prepareRouterTransaction(connection: Connection, transaction: Transaction, options?: ConfirmOptions): Promise<Transaction> {
@@ -32,7 +50,7 @@ export async function prepareRouterTransaction(connection: Connection, transacti
       params: [writableAccounts]
     })
   });
-  
+
   const blockHashData = await blockHashResponse.json();
   transaction.recentBlockhash = blockHashData.result.blockhash;
 
@@ -51,7 +69,7 @@ export async function sendRouterTransaction(connection: Connection, transaction:
       params: [writableAccounts]
     })
   });
-  
+
   const blockHashData = await blockHashResponse.json();
   transaction.recentBlockhash = blockHashData.result.blockhash;
   transaction.feePayer = signers[0].publicKey;
